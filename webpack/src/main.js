f827e8a47fd1b6d8a05de33b5cffa81a00077e8c
gordonshin <shenchen1250@hotmail.com>
2019-11-04 15:08:14 +0800

    Initial commit

---
+++ webpack/src/main.js
@@ -0,0 +1,4 @@
+import * as math from './mathUtils.js'
+
+console.log(math.add(12, 23))
+console.log(math.multi(70,60))
---
+++ webpack/webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path')
+module.exports={
+    entry:'',
+    output:{
+        path:'',
+        filename:'bundle.js'
+    }
+}
